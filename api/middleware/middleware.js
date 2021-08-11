26ab40f2db86d0d3d2bd1e72a17d761491cc5087
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-08-11 02:42:12 -0400

    mvp

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,47 @@
+
+const {getById} = require('../users/users-model');
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+
+  const {method, url} = req;
+  const timestamp = Date.now().toLocaleString();
+
+  console.log(`Request Method: ${method}`);
+  console.log(`Request Url: ${url}`);
+  console.log(`Timestamp: `);
+
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+
+  const {id} = req.params;
+
+  try {
+    const user = await getById(id);
+    if (!user) throw user;
+    req.user = user;
+    next();
+  } catch(err) {
+    res.status(404).json({message: 'user not found'});
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const newUser = req.body;
+  if (!newUser.name) res.status(400).json({message: 'missing required text field'});
+  next();
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  const newUser = req.body;
+  if (!newUser.text) res.status(400).json({message: `missing required text field`});
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
